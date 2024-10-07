4282ac9e0ca4c8555001ef873809b2ddb17d11f6
cometas321 <renerolando321@gmil.com>
2024-10-07 16:13:47 -0500

    php

--- 01/cliente/src/services/index.js
+++ 01/cliente/src/services/index.js
@@ -1,4 +1,4 @@
-const API_SERVER_1 = "http://api-system.endogastropuno.com/users";
+const API_SERVER_1 = "https://api-system.endogastropuno.com/users";
 const API_SERVER_2 = "https://distributed-system.onrender.com/api/profesor";
 const API_SERVER_3 =
   "https://api-calificaciones.onrender.com/api/calificaciones";
